54bd484e6a89ba789b76b7305fc8bb2b52deb8ac
NKAndrei <andrei.iuliannechita@gmail.com>
2021-11-10 17:29:18 +0000

    Lab3 and a small part of lab 4

---
+++ DoghnutMachine.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Threading;
+
+namespace Nechita_Andrei_Lab2
+{
+    class DoghnutMachine
+    {
+        private DoghnutType mFlavor;
+
+        public DoghnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private System.Collections.ArrayList mDoghnuts = new System.Collections.ArrayList();
+        public Doghnut this[int Index]
+        {
+            get
+            {
+                return (Doghnut)mDoghnuts[Index];
+            }
+            set
+            {
+                mDoghnuts[Index] = value;
+            }
+        }
+
+        public delegate void DoghnutCompleteDelegate();
+        public event DoghnutCompleteDelegate DoghnutComplete;
+
+        DispatcherTimer doghnutTimer;
+
+        private void InitializaComponent()
+        {
+            this.doghnutTimer = new DispatcherTimer();
+            this.doghnutTimer.Tick += new System.EventHandler(this.doghnutTimer_Tick);
+        }
+
+        public DoghnutMachine()
+        {
+            InitializaComponent();
+        }
+
+        private void doghnutTimer_Tick(object sender, EventArgs e)
+        {
+            Doghnut aDoghnut = new Doghnut(this.Flavor);
+            mDoghnuts.Add(aDoghnut);
+            DoghnutComplete();
+        }
+
+        public bool Enabled
+        {
+            set
+            {
+                doghnutTimer.IsEnabled = value;
+            }
+        }
+        public int Interval
+        {
+            set
+            {
+                doghnutTimer.Interval = new TimeSpan(0,0,value);
+            }
+        }
+
+        public void MakeDoghnuts(DoghnutType dFlavor)
+        {
+            Flavor = dFlavor;
+            switch (Flavor)
+            {
+                case DoghnutType.Glazed: Interval = 3; break;
+                case DoghnutType.Sugar: Interval = 2; break;
+                case DoghnutType.Lemon: Interval = 5; break;
+                case DoghnutType.Chocolate: Interval = 7; break;
+                case DoghnutType.Vanilla: Interval = 4; break;
+            }
+        }
+    }
+    public enum DoghnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+    class Doghnut
+    {
+        private DoghnutType mFlavor;
+        public DoghnutType Flavor{
+            get{
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+
+        private float mPrice = .50F;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+
+        private readonly DateTime mTimeOfCreation;
+        public DateTime TimeOfCreation
+        {
+            get
+            {
+                return mTimeOfCreation;
+            }
+        }
+
+        public Doghnut(DoghnutType aFlavor)
+        {
+            mTimeOfCreation = DateTime.Now;
+            mFlavor = aFlavor;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,13 @@ namespace Nechita_Andrei_Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DoghnutMachine myDoghnutMachine;
+
+        private int mRaisedGlazed;
+        private int mRaisedSugar;
+        private int mFilledLemon;
+        private int mFilledChocolate;
+        private int mFilledVanilla;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,5 +37,100 @@ namespace Nechita_Andrei_Lab2
             //RoutedEventArgs base class that handles all events, all other event handling classes are child classes of this one
             // sender is the button that actually calls this method
         }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            myDoghnutMachine = new DoghnutMachine();
+            myDoghnutMachine.DoghnutComplete += new DoghnutMachine.DoghnutCompleteDelegate(DoghnutCompleteHandler);
+        }
+
+        private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = true;
+            sugarToolStripMenuItem.IsChecked = false;
+            myDoghnutMachine.MakeDoghnuts(DoghnutType.Glazed);
+        }
+
+        private void sugarToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = false;
+            sugarToolStripMenuItem.IsChecked = true;
+            myDoghnutMachine.MakeDoghnuts(DoghnutType.Sugar);
+        }
+        private void lemonFilledMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            lemonFilledMenuItem.IsChecked = true;
+            chocolateFilledMenuItem.IsChecked = false;
+            vanillaFilledMenuItem.IsChecked = false;
+            myDoghnutMachine.MakeDoghnuts(DoghnutType.Lemon);
+        }
+
+        private void chocolateFilledMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            lemonFilledMenuItem.IsChecked = true;
+            chocolateFilledMenuItem.IsChecked = false;
+            vanillaFilledMenuItem.IsChecked = false;
+            myDoghnutMachine.MakeDoghnuts(DoghnutType.Chocolate);
+        }
+
+        private void vanillaFilledMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            lemonFilledMenuItem.IsChecked = true;
+            chocolateFilledMenuItem.IsChecked = false;
+            vanillaFilledMenuItem.IsChecked = false;
+            myDoghnutMachine.MakeDoghnuts(DoghnutType.Vanilla);
+        }
+        private void exitToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myDoghnutMachine.Enabled = false;
+        }
+
+        private void DoghnutCompleteHandler()
+        {
+            switch (myDoghnutMachine.Flavor)
+            {
+                case DoghnutType.Glazed:
+                    mRaisedGlazed++;
+                    txtGlazedRaised.Text = mRaisedGlazed.ToString();
+                    break;
+
+                case DoghnutType.Sugar:
+                    mRaisedSugar++;
+                    txtSugarRaised.Text = mRaisedSugar.ToString();
+                    break;
+                case DoghnutType.Lemon:
+                    mFilledLemon++;
+                    txtLemonFilled.Text = mFilledLemon.ToString();
+                    break;
+                case DoghnutType.Chocolate:
+                    mFilledChocolate++;
+                    txtChocolateFilled.Text = mFilledChocolate.ToString();
+                    break;
+                case DoghnutType.Vanilla:
+                    mFilledVanilla++;
+                    txtVanillaFilled.Text = mFilledVanilla.ToString();
+                    break;
+            }
+        }
+
+        private void txtQuantity_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (!(e.Key>=Key.D0 && e.Key<=Key.D9))
+            {
+                MessageBox.Show("Numaicifre se pot introduce!", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        
+        //!!!!!!!!!!!!!-----------NEED TO ADD THE REST SUCH AS VANILLA CHOCOLATE ETC BUTTONS, VALUES AND FUNCTIONALITIES
+        //private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //  myDoghnutMachine = new DoghnutMachine();
+        // }
     }
 }
