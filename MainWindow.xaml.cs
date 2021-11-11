b921debd1c5fa09d13960e5c36150e7d6d9378e3
NKAndrei <andrei.iuliannechita@gmail.com>
2021-11-11 16:14:08 +0000

    Lab4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,9 +27,40 @@ namespace Nechita_Andrei_Lab2
         private int mFilledLemon;
         private int mFilledChocolate;
         private int mFilledVanilla;
+
+        KeyValuePair<DoghnutType, double>[] PriceList =
+        {
+            new KeyValuePair<DoghnutType, double>(DoghnutType.Sugar, 2.5),
+            new KeyValuePair<DoghnutType, double>(DoghnutType.Glazed, 3),
+            new KeyValuePair<DoghnutType, double>(DoghnutType.Chocolate, 4.5),
+            new KeyValuePair<DoghnutType, double>(DoghnutType.Vanilla, 4),
+            new KeyValuePair<DoghnutType, double>(DoghnutType.Lemon, 3.5),
+
+        };
+        DoghnutType selectedDoghnut;
         public MainWindow()
         {
             InitializeComponent();
+            CommandBinding cmd1 = new CommandBinding();
+            cmd1.Command = ApplicationCommands.Print;
+            ApplicationCommands.Print.InputGestures.Add(new KeyGesture(Key.I, ModifierKeys.Alt));
+            cmd1.Executed += new ExecutedRoutedEventHandler(CtrlP_CommandHandler);
+            this.CommandBindings.Add(cmd1);
+
+            CommandBinding cmd2 = new CommandBinding();
+            cmd2.Command = CustomCommands.StopCommand.Launch;
+            cmd2.Executed += new ExecutedRoutedEventHandler(CtrlS_CommandHandler);
+            this.CommandBindings.Add(cmd2);
+        }
+        private void CtrlP_CommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You have in stock:"+mRaisedGlazed+" Glazed," +mRaisedSugar+
+            "Sugar," +mFilledLemon+ "Lemon," +mFilledChocolate+ "Chocolate," +mFilledVanilla+ "Vanilla");
+        }
+        private void CtrlS_CommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Ctrl+S was pressed! the doghnut machine will stop!");
+            this.stopToolStripMenuItem_Click(sender, e);
         }
 
         private void btnButon1_Click(object sender, RoutedEventArgs e)
@@ -42,6 +73,10 @@ namespace Nechita_Andrei_Lab2
         {
             myDoghnutMachine = new DoghnutMachine();
             myDoghnutMachine.DoghnutComplete += new DoghnutMachine.DoghnutCompleteDelegate(DoghnutCompleteHandler);
+
+            cmbType.ItemsSource = PriceList;
+            cmbType.DisplayMemberPath = "Key";
+            cmbType.SelectedValuePath = "Value";
         }
 
         private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
@@ -57,6 +92,7 @@ namespace Nechita_Andrei_Lab2
             sugarToolStripMenuItem.IsChecked = true;
             myDoghnutMachine.MakeDoghnuts(DoghnutType.Sugar);
         }
+        /*
         private void lemonFilledMenuItem_Click(object sender, RoutedEventArgs e)
         {
             lemonFilledMenuItem.IsChecked = true;
@@ -80,6 +116,7 @@ namespace Nechita_Andrei_Lab2
             vanillaFilledMenuItem.IsChecked = false;
             myDoghnutMachine.MakeDoghnuts(DoghnutType.Vanilla);
         }
+        */
         private void exitToolStripMenuItem_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
@@ -88,6 +125,8 @@ namespace Nechita_Andrei_Lab2
         private void stopToolStripMenuItem_Click(object sender, RoutedEventArgs e)
         {
             myDoghnutMachine.Enabled = false;
+            this.Title = " Virtual Doghnuts Factory";
+            e.Handled = true;
         }
 
         private void DoghnutCompleteHandler()
@@ -126,7 +165,122 @@ namespace Nechita_Andrei_Lab2
             }
         }
 
-        
+        private void FilledItems_Click(object sender, RoutedEventArgs e)
+        {
+            String DoghnutFlavour;
+            MenuItem SelectedItem = (MenuItem)e.OriginalSource;
+            DoghnutFlavour = SelectedItem.Header.ToString();
+
+            Enum.TryParse(DoghnutFlavour, out DoghnutType myFlavour);
+            myDoghnutMachine.MakeDoghnuts(myFlavour);
+        }
+        private void FilledItemsShow_Click(object sender, RoutedEventArgs e)
+        {
+            string mesaj;
+
+            MenuItem SelectedItem = (MenuItem)e.OriginalSource;
+            mesaj = SelectedItem.Header.ToString() + " doghnuts are being cooked";
+            this.Title = mesaj;
+        }
+
+        private void cmbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtPrice.Text = cmbType.SelectedValue.ToString();
+            KeyValuePair<DoghnutType, double> selectedEntry = (KeyValuePair<DoghnutType, double>)cmbType.SelectedItem;
+            selectedDoghnut = selectedEntry.Key;
+        }
+
+        private int ValidateQuantity(DoghnutType selectedDoghnut)
+        {
+            int q = int.Parse(txtQuantity.Text);
+            int r = 1;
+
+            switch (selectedDoghnut)
+            {
+                case DoghnutType.Glazed:
+                    if (q > mRaisedGlazed)
+                    {
+                        r = 0;
+                    }
+                    break;
+                case DoghnutType.Sugar:
+                    if (q > mRaisedGlazed)
+                    {
+                        r = 0;
+                    }
+                    break;
+                case DoghnutType.Chocolate:
+                    if (q > mFilledChocolate)
+                    {
+                        r = 0;
+                    }
+                    break;
+                case DoghnutType.Lemon:
+                    if (q > mFilledLemon)
+                    {
+                        r = 0;
+                    }
+                    break;
+                case DoghnutType.Vanilla:
+                    if (q > mFilledVanilla)
+                    {
+                        r = 0;
+                    }
+                    break;
+            }
+
+            return r;
+        }
+
+        private void btnAddToSale_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateQuantity(selectedDoghnut) > 0)
+            {
+                lstSale.Items.Add(txtQuantity.Text + " " + selectedDoghnut.ToString() + ":" + txtPrice.Text + " " + double.Parse(txtQuantity.Text) * double.Parse(txtPrice.Text));
+            }
+            else
+            {
+                MessageBox.Show("Cantitatea introdusa nu este disponibila in stoc!");
+            }
+        }
+
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            lstSale.Items.Remove(lstSale.SelectedItem);
+        }
+
+        private void btnCheckOut_Click(object sender, RoutedEventArgs e)
+        {
+            txtTotal.Text = (double.Parse(txtTotal.Text) + double.Parse(txtQuantity.Text) * double.Parse(txtPrice.Text)).ToString();
+
+            foreach (string s in lstSale.Items)
+            {
+                switch (s.Substring(s.IndexOf(" ") + 1, s.IndexOf(":") - s.IndexOf(" ") - 1))
+                {
+                    case "Glazed":
+                        mRaisedGlazed = mRaisedGlazed - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtGlazedRaised.Text = mRaisedGlazed.ToString();
+                        break;
+                    case "Sugar":
+                        mRaisedSugar = mRaisedSugar - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtSugarRaised.Text = mRaisedSugar.ToString();
+                        break;
+                    case "Chocolate":
+                        mFilledChocolate = mFilledChocolate - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtChocolateFilled.Text = mFilledChocolate.ToString();
+                        break;
+                    case "Vanilla":
+                        mFilledVanilla = mFilledVanilla - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtVanillaFilled.Text = mFilledVanilla.ToString();
+                        break;
+                    case "Lemon":
+                        mFilledLemon = mFilledLemon - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtLemonFilled.Text = mFilledLemon.ToString();
+                        break;
+                }
+
+            }
+        }
         //!!!!!!!!!!!!!-----------NEED TO ADD THE REST SUCH AS VANILLA CHOCOLATE ETC BUTTONS, VALUES AND FUNCTIONALITIES
         //private void frmMain_Loaded(object sender, RoutedEventArgs e)
         //{
